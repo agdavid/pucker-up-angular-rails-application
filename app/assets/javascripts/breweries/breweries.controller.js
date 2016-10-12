472d1568d3ad336aff9008a4d1e954dfcee77865
agdavid <antonio.g.david@gmail.com>
2016-10-12 09:42:19 -0400

    add button to resetBreweries to full list

--- app/assets/javascripts/breweries/breweries.controller.js
+++ app/assets/javascripts/breweries/breweries.controller.js
@@ -10,6 +10,7 @@
         vm.refilter = refilter;
         vm.createBrewery = createBrewery;
         vm.signedIn = Auth.isAuthenticated();
+        vm.resetBreweries = resetBreweries;
 
         //instantiated info
         activate();
@@ -59,6 +60,10 @@
 
         function showBrewery(data) {
             $state.go('home.show', { breweryId: data.id });
+        };
+
+        function resetBreweries() {
+            return vm.filteredList = vm.breweries;
         };
 
     };
